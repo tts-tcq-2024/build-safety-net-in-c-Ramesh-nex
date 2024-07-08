930af0a9057ed2a690a14260eafd15845160b17a
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-08 09:50:34 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,21 +5,9 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
- ASSERT_STREQ(soundex,"A200");
-}
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("E2CY", soundex);
- ASSERT_STREQ(soundex,"A414");
+ ASSERT_EQ(soundex,"A200");
 }
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("X/", soundex);
-
-}
 
 
 
