ec7d4957cf4aa1597eb79c97c3d5d80ad9c38121
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-18 14:38:16 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -5,14 +5,21 @@
 #include <ctype.h>
 #include <string.h>
 
+int checkalphabet(c)
+{
+    if (c < 'A' || c > 'Z') {
+        return 1;
+    }
+}
 char getSoundexCode(char c) {
     static const char soundexTable[26] = {'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5','5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
     };
     c = toupper(c);
-    if (c < 'A' || c > 'Z') {
-        return '0';
+    if (checkalphabet(c))
+    {
+        return soundexTable[c - 'A'];
     }
-    return soundexTable[c - 'A'];
+     return '0';
 }
 
 void initializeSoundex(char *soundex, char firstCharacter) {
