a05d30debae6a906ce770eff8be92ac2fda2d24e
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-18 15:00:37 +0530

    aaa

--- Soundex.h
+++ Soundex.h
@@ -5,17 +5,18 @@
 #include <ctype.h>
 #include <string.h>
 
-int checkalphabet(c)
+int isalphabet(c)
 {
     if (c < 'A' || c > 'Z') {
         return 1;
     }
+    return 0;
 }
 char getSoundexCode(char c) {
     static const char soundexTable[26] = {'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5','5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
     };
-    char c1 = toupper(c);
-    if (checkalphabet(c1))
+    c = toupper(c);
+    if (isalphabet(c))
     {
         return soundexTable[c - 'A'];
     }
