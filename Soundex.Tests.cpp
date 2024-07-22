d4cc52772b31f663c68d9b1d99fd5577524e85f3
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-22 13:51:47 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -23,9 +23,21 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
   generateSoundex("A[", soundex);
   ASSERT_STREQ(soundex,"A000");
 }
-
-//Replace all adjacent same digits with one digit
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
+ //AAA
+  char soundex[5];
+  generateSoundex("ACGJ", soundex);
+ ASSERT_STREQ(soundex,"A200");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
+ //AAA
+  char soundex[5];
+  generateSoundex("@@BBCCDD", soundex);
+ ASSERT_STREQ(soundex,"@123");
+}
+//Replace all adjacent same digits with one digit
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
 //AAA
   char soundex[5];
   generateSoundex("Abfp", soundex);
@@ -33,7 +45,7 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_4) {
 }
 
 //two letters with the same number separated by 'h', 'w' or 'y' are coded as a single number
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_7) {
 //AAA
   char soundex[5];
   generateSoundex("BShZ", soundex);
